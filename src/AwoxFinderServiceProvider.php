11c19ad4f4ed6b25c9884a11ff83942deb5d8432
Vladimir Bratukhin <insyon@gmail.com>
2022-01-04 19:39:49 +0000

    First commit

---
+++ src/AwoxFinderServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Vo1\Seat\AwoxFinder;
+
+use Seat\Services\AbstractSeatPlugin;
+
+class AwoxServiceProvider extends AbstractSeatPlugin
+{
+    public function boot()
+    {
+    }
+
+    public function getName(): string
+    {
+        return 'AwoxFinder';
+    }
+
+    public function getPackageRepositoryUrl(): string
+    {
+        return 'https://github.com/vo1/seat-awox-finder-discord';
+    }
+
+    public function getPackagistPackageName(): string
+    {
+        return 'vo1/seat-awox-finder-discord';
+    }
+
+    public function getPackagistVendorName(): string
+    {
+        return 'vo1';
+    }
+}
